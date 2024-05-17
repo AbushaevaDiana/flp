bc20d57efa6734ebf0f658d2f808467274956db1
AbushaevaDiana <79981087+AbushaevaDiana@users.noreply.github.com>
2024-05-17 17:50:46 +0300

    Update ts2.hs

--- Haskell /Теема2/ts2.hs
+++ Haskell /Теема2/ts2.hs
@@ -1,12 +1,14 @@
 import System.IO
 -- insert (L,A,n) - функция включения в список L заданного атома А на определенную позицию n
 insert :: [a] -> a -> Int -> [a]
-insert l a n = (take n l) ++ [a] ++ (drop n l)
+insert l a n 
+    | n < 0 = error "Negative index is not possible for an array"
+    | otherwise = (take n l) ++ [a] ++ (drop n l)
 
 main :: IO ()
 
 main = do
   let list = [1, 3, 4, 5]
-  let atom = 2
-  let n = 1
+  let atom = -2
+  let n = -1
   print( insert list atom n )
