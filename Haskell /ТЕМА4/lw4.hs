3babce2ae078ebd9391d776a937b6890095965a1
AbushaevaDiana <79981087+AbushaevaDiana@users.noreply.github.com>
2024-05-17 10:48:56 +0300

    Update lw4.hs

--- Haskell /ТЕМА4/lw4.hs
+++ Haskell /ТЕМА4/lw4.hs
@@ -5,15 +5,15 @@ import System.IO
 myTranspose :: [[a]] -> [[a]]
 myTranspose [] = []
 myTranspose ([]:_) = []
-myTranspose x = (map head x) : myTranspose (map tail x)
+myTranspose x = map head x : myTranspose (map tail x)
 
 main :: IO ()
 
 main = do
-  let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-  print $ myTranspose matrix
+  let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9, 8]]
+  print(myTranspose matrix)
 -- Ожидаемый вывод: [[1,4,7],[2,5,8],[3,6,9]]
 
-  let nonSquareMatrix = [[1, 2], [3, 4], [5, 6]]
-  print $ myTranspose nonSquareMatrix
+  let nonSquareMatrix = ["hey","the","guy"]
+  print (myTranspose nonSquareMatrix)
 -- Ожидаемый вывод: [[1,3,5],[2,4,6]]
