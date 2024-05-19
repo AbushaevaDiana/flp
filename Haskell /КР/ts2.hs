c31d3f167c22db47d97079a4efa072c7383a3ec1
AbushaevaDiana <79981087+AbushaevaDiana@users.noreply.github.com>
2024-05-19 18:53:10 +0300

    Update ts2.hs

--- Haskell /КР/ts2.hs
+++ Haskell /КР/ts2.hs
@@ -1,17 +1,23 @@
 import System.IO
+import Data.List
 -- 2. Пифагорова тройка это три такие натуральные числа a < b < c, 
 -- что a^2 + b^2 = c^2 Например, 3^2 + 4^2 = 9 + 16 = 25 = 5^2. 
--- Найдите единственную Пифагорову тройку, 
--- такую что a + b + c = 1000 и вычислите произведение abc. #5
+-- Найдите единственную Пифагорову тройку, такую что a + b + c = 1000 
+-- и вычислите произведение abc. #5
 
---Пифогорова тройка применима для прямоугольного треугольника. 
--- Следовательно 1000 - это периметр
-myFunc :: Int -> Int
-myFunc x = sum $ filter (\x -> (mod x 3 == 0) || (mod x 5 == 0)) [1..x-1]
-
+findPythagoreanTriplet :: Int -> (Int, Int, Int)
+findPythagoreanTriplet sumOfAbc =
+    head [(a, b, c) | 
+        a <- [1 .. sumOfAbc],
+        b <- [a + 1 .. sumOfAbc],
+        let c = sumOfAbc - (a + b),
+        a ^ 2 + b ^ 2 == c ^ 2
+        ]
 
 main :: IO ()
-
 main = do
-  print(myFunc 1000)
---Ожидаемый результат 233168
+    let (a, b, c) = findPythagoreanTriplet 1000
+    putStrLn "Пифагорова тройка: "
+    print(a, b, c)
+    putStrLn $ "Произведение abc: " 
+    print(a * b * c)
