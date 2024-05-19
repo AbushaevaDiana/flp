2fb5d3508575bdb037517720974426731b8d1b77
AbushaevaDiana <79981087+AbushaevaDiana@users.noreply.github.com>
2024-05-19 18:57:20 +0300

    Update ts3.hs

--- Haskell /КР/ts3.hs
+++ Haskell /КР/ts3.hs
@@ -1,8 +1,14 @@
 import System.IO
-import qualified Data.Map as Map
 
 -- Собственная реализация функции transpose
 myTranspose :: [[a]] -> [[a]]
 myTranspose [] = []
 myTranspose ([]:_) = []
 myTranspose x = map head x : myTranspose (map tail x)
+
+main :: IO ()
+
+main = do
+  let matrix = [[1,2,3],[4,5,6]]
+  print(myTranspose matrix)
+-- Ожидаемый вывод: [[1,4],[2,5],[3,6]]
